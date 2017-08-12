8119b99187c0246f423fc34d5e624c78e0cad453
Edward <graphicartgeek@gmail.com>
2017-08-11 18:54:50 -0700

    update mailchimp plugin

--- wp-content/plugins/mailchimp-for-wp/vendor/autoload_52.php
+++ wp-content/plugins/mailchimp-for-wp/vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInitef76aa1bb6f3ca98f87763d7c82e66ab::getLoader();
+return ComposerAutoloaderInit31db873dcde1a94f0b3b238770a91c24::getLoader();
