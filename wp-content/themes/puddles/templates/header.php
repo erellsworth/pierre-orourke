38b0886b9a5de8935dc9962ee7e070a342be698b
Edward <graphicartgeek@gmail.com>
2017-08-13 08:00:27 -0700

    menu styling and full page tempalte

--- wp-content/themes/puddles/templates/header.php
+++ wp-content/themes/puddles/templates/header.php
@@ -4,7 +4,10 @@
     <nav class="nav-primary" role="navigation">
       <?php
       if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav nav-pills justify-content-center',  'walker' => new wp_bootstrap_navwalker()]);
+        wp_nav_menu([
+                'theme_location' => 'primary_navigation', 
+                'menu_class' => 'nav nav-pills justify-content-center flex-column flex-sm-row',
+                'walker' => new bootstrap_4_walker_nav_menu()]);
       endif;
       ?>
     </nav>
