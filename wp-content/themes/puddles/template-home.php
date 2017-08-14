f9d07b2cdbcf93588b775e7b22bc30abbf64237e
Edward <graphicartgeek@gmail.com>
2017-08-14 06:43:54 -0700

    show 3 latest posts on home page

--- wp-content/themes/puddles/template-home.php
+++ wp-content/themes/puddles/template-home.php
@@ -7,3 +7,15 @@
 <?php while (have_posts()) : the_post(); ?>
   <?php get_template_part('templates/content', 'page'); ?>
 <?php endwhile; ?>
+
+<h3>Latest</h3>
+<?php 
+$args = array('post_status' => 'publish', 'posts_per_page' => 3);
+
+$the_query = new WP_Query($args);
+while ( $the_query->have_posts()) :  $the_query->the_post(); ?>
+	<?php get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
+<?php endwhile;
+ wp_reset_postdata(); 
+ ?>
+
