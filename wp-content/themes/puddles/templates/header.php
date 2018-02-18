b4b5d9d68cacec80913331de1039b9277956641c
Edward <graphicartgeek@gmail.com>
2018-02-17 22:12:10 -0700

    working with menu

--- wp-content/themes/puddles/templates/header.php
+++ wp-content/themes/puddles/templates/header.php
@@ -3,16 +3,26 @@ use Roots\Sage\Assets;
 ?>
 <header class="banner">
   <div class="container">
-    <nav class="nav-primary" role="navigation">
-      <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu([
-                'theme_location' => 'primary_navigation', 
-                'menu_class' => 'nav nav-pills justify-content-center flex-column flex-sm-row',
-                'walker' => new bootstrap_4_walker_nav_menu()]);
-      endif;
-      ?>
-    </nav>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-content" aria-controls="navbar-content" aria-expanded="true" aria-label="<?php esc_html_e( 'Toggle Navigation', 'theme-textdomain' ); ?>">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbar-content">
+        <?php
+        wp_nav_menu( array(
+          'theme_location' => 'primary_navigation', // Defined when registering the menu
+          'menu_id'        => 'primary-menu',
+          'container'      => false,
+          'depth'          => 2,
+          'menu_class'     => 'navbar-nav ml-auto',
+          'walker'         => new Bootstrap_NavWalker(), // This controls the display of the Bootstrap Navbar
+          'fallback_cb'    => 'Bootstrap_NavWalker::fallback', // For menu fallback
+        ) );
+        ?>
+      </div>
+    </nav>    
     <a class="brand" href="<?= esc_url(home_url('/')); ?>">
       <img src="<?= Assets\asset_path('images/logo.svg'); ?>" />
     </a>    
