07dcad32c33c85e1021558720e6858a3d9602e11
Edward <graphicartgeek@gmail.com>
2018-02-19 21:11:20 -0700

    footer

--- wp-content/themes/puddles/templates/footer.php
+++ wp-content/themes/puddles/templates/footer.php
@@ -1,5 +1,6 @@
 <footer class="content-info">
   <div class="container">
     <?php dynamic_sidebar('sidebar-footer'); ?>
+  	<p class="text-center copyright_notice">Copyright <?php echo Date('Y'); ?> Pierre O'Rourke</p>
   </div>
 </footer>
