c3d6a76cfb288dc46418fa27d3fe7c1c34064e5f
Edward <graphicartgeek@gmail.com>
2018-02-27 20:42:50 -0700

    social media links in footer

--- wp-content/themes/puddles/templates/footer.php
+++ wp-content/themes/puddles/templates/footer.php
@@ -1,6 +1,11 @@
+<?php use Roots\Sage\Assets; ?>
 <footer class="content-info">
   <div class="container">
     <?php dynamic_sidebar('sidebar-footer'); ?>
-  	<p class="text-center copyright_notice">Copyright <?php echo Date('Y'); ?> Pierre O'Rourke</p>
+    <div class="text-center">
+    	<a href="https://www.facebook.com/PierreORourkeAuthor/" target="_blank"><img src="<?= Assets\asset_path('images/facebook.png'); ?>" alt="Pierre O'Rourke on Facebook" /></a>
+    	<a href="https://twitter.com/PierreORourke" target="_blank"><img src="<?= Assets\asset_path('images/twitter.png'); ?>" alt="Pierre O'Rourke on Twitter" /></a>
+  		<p class="copyright_notice">Copyright <?php echo Date('Y'); ?> Pierre O'Rourke</p>
+  	</div>
   </div>
 </footer>
